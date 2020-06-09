9d38a7a4429c8023571e4ef4b56758574629dc69
Piotr Gogola <gogola.piotr@interia.pl>
2020-06-09 18:42:00 +0200

    Fix generate data script

--- generate_data.sh
+++ generate_data.sh
@@ -2,14 +2,19 @@
 
 source env/bin/activate
 
-python ./scripts/generate_data.py \
+echo "Start generate cars"
+python ./generate_data.py \
     "./resources/pa3_images/train/car" \
     "./resources/pa3_images/train_augmentation/car" \
     8000
 
-python ./scripts/generate_data.py \
+
+echo "Start generate others"
+python ./generate_data.py \
     "./resources/pa3_images/train/other" \
     "./resources/pa3_images/train_augmentation/other" \
     8000
+
+echo "Finished"
 
 deactivate

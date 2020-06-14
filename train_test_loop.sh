0c4bc484c50d86b23b74380cd002e96d2e9b1bbe
Piotr Gogola <gogola.piotr@interia.pl>
2020-06-14 23:27:27 +0200

    Add single epoch run

--- train_test_loop.sh
+++ train_test_loop.sh
@@ -2,28 +2,36 @@
 
 source ./env/bin/activate
 
-lrs=("0.01" "0.0001")
-lrs_str=("001" "00001")
+# resnet 244 400 20 0,0001
+# vgg16 128 256 20 0,0001
+# simple 128 128 20 0,001
 
+models_list=("resnet" "vgg16" "simple")
+lrs=("0.0001" "0.0001" "0.001")
+batches_list=(400 256 128)
+epochs_list=(20 20 20)
+img_size_list=(244 128 126)
+
+pretrained_model_dir="./pretrained_models"
 
 for i in ${!lrs[@]};
 do
     lr_param=${lrs[$i]}
-    lr_param_str=${lrs_str[$i]}
+    model=${models_list[$i]}
+    epochs=${epochs_list[$i]}
+    batch=${batches_list[$i]}
+    img_size=${img_size_list[$i]}
 
-    python ./train.py \
-        --model simple \
+    python ./train_single_loop.py \
+        --model $model \
         --train-dir ./resources/pa3_images/train_augmentation/ \
         --val-dir ./resources/pa3_images/validation/ \
-        --model-h5 ./model.h5 \
-        --model-json ./model.json \
-        --epochs 20 \
+        --model-h5 ./$pretrained_model_dir/$model.h5 \
+        --model-json ./$pretrained_model_dir/$model.json \
+        --epochs $epochs \
         --lr $lr_param \
-        --batch-size 128 \
-        --img-size 64 \
-        --save-history "./history"$lr_param_str".hist" \
-        --save-plot "./plot"$lr_param_str".png"
-        # --show-plot
+        --batch-size $batch \
+        --img-size $img_size
 
 done
 deactivate
